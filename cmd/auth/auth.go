29473e5dc7fda27ba7c8c6277a95ca335192b098
Stephen <appcypher@outlook.com>
2021-02-10 16:30:40 +0100

    Refactor project to use secrets manager and config

--- cmd/auth/auth.go
+++ cmd/auth/auth.go
@@ -1,22 +1,37 @@
 package main
 
 import (
+	"github.com/sageflow/sageflow/pkg/secrets"
 	"github.com/sageflow/sageflow/pkg/database"
-	"github.com/sageflow/sageflow/pkg/envs"
+	"github.com/sageflow/sageflow/pkg/configs"
 	"github.com/sageflow/sageflow/pkg/logs"
 
 	"github.com/sageflow/sageauth/pkg/server"
 )
 
 func main() {
-	// Set up log status file and load .env file.
-	logs.SetStatusLogFile() // TODO. logs.SetStatusLogFile(config.Logging.Status.Filepath)
-	envs.LoadEnvFile()      // TODO. Remove!
+	// Set up log status file.
+	logs.SetStatusLogFile()
+
+	// Load sageflow config file.
+	config, err := configs.LoadSageflowConfig()
+	if err != nil {
+		logs.FmtPrintln("Unable to load config file:", err)
+	}
+
+	// Set up secret manager,
+	secrets, err := secrets.NewManager(&config)
+	if err != nil {
+		logs.FmtPrintln("Unable to create secrets manager:", err)
+	}
 
 	// Connect to database.
-	db := database.Connect() // TODO. database.Connect(config.db)
+	db, err := database.Connect(secrets)
+	if err != nil {
+		logs.FmtPrintln("Unable to connect to database:", err)
+	}
 
 	// Start an auth gRPC server.
-	server := server.NewAuthServer(db) // TODO. server.NewAuthServer(db, config)
+	server := server.NewAuthServer(&db) // TODO. server.NewAuthServer(db, config)
 	server.Listen("3003") // TODO. server.Listen(config.Server.Engine.Port)
 }
--- go.mod
+++ go.mod
@@ -10,3 +10,5 @@ require (
 	google.golang.org/grpc v1.34.0
 	google.golang.org/protobuf v1.25.0
 )
+
+replace github.com/sageflow/sageflow v0.0.0-20210209165522-8b9455bbe20d => ../sageflow
